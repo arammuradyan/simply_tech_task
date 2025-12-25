68d31c9613772241b0ac5eb039e6bb52e5786496
arammuradyan <aram.muradyan2089@gmail.com>
2025-12-25 11:53:40 +0400

    test commit

--- app/src/main/java/com/example/simplytask/MainActivity.kt
+++ app/src/main/java/com/example/simplytask/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.WindowInsetsControllerCompat
